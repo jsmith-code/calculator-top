eb8cb6014709c9b0475cf280c9d10687998c7901
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-09-05 14:24:05 -0600

    refac(calc): Remove unnecessary calculation functions

--- scripts.js
+++ scripts.js
@@ -105,30 +105,14 @@ function operate(a, b, operator) {
 
     switch (operator) {
         case "+":
-            return add(a, b);
+            return a + b;
         case "-":
-            return subtract(a, b);
+            return a - b;
         case "*":
-            return multiply(a, b);
+            return a * b;
         case "/":
-            return divide(a, b);
+            return a / b;
     }
-}
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
 }
 
 const digits = "0123456789";
